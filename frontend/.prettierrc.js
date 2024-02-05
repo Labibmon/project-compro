38043de52ae92de8d20f62888e6f408a993a6d81
Muhammad Anova Nurfaqih <49178784+muhammadanova@users.noreply.github.com>
2024-02-05 14:48:55 +0700

    feat: setup husky commitlint rules eslint and prettier (#3)
    * feat(FE): Init husky and github workflow and pull request
    
    * feat: added depedencies husky etc
    
    * fix: remove unused
    
    * fix: foldering github
    
    * fix: move folder
    
    * fix: setup package json
    
    * fix: setup package
    
    * fix: pre commit
    
    * fix: testing
    
    * fix: testing commit
    
    * fix: yml syntax
    
    * fix: workflow
    
    * fix: multiple command run
    
    * fix: steps
    
    * fix: included yarn lock
    
    ---------
    
    Co-authored-by: Anova <mnurfaqih@hijra.id>

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 100,
+  endOfLine: 'auto',
+};
